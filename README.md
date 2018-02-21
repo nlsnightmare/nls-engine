8865113103dbffda6bc8041c6d3e2f6f8fe3f58f
Nlsnightmare < >
2018-02-21 22:04:06 +0200

    Deleted stuff

--- README.md
+++
@@ -1,2 +0,0 @@
-# nls-engine
-A 3D rendering engine using OpenGL
--- test.lua
+++
@@ -1,7 +0,0 @@
-angle = 0
-
-function update(dt)
-   __transform:rot(angle)
-   __transform:scale(2,2,1)
-   angle = angle + 10 * dt
-end
